c26398dc3566251b0d4fc86d73b819f53d713549
Arthur Azevedo <arthuraz.1990@gmail.com>
2023-11-17 16:03:25 -0300

    Atualização na página de carregamento

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,11 +1,14 @@
 import { ActivityIndicator, Dimensions, StyleSheet, View } from "react-native";
 import CustomText from "../components/element/CustomText";
 import FontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
 
 const LoadingScreen = ({ textStyle }) => (
-    <View style={styles.container}>
-        <ActivityIndicator size='large'/>
-        <CustomText bold style={[styles.text, textStyle]}>Carregando...</CustomText>
+    <View style={styles.mainContainer}>
+        <View style={styles.borderContainer}>
+            <ActivityIndicator size='large'/>
+            <CustomText bold style={[styles.text, textStyle]}>Carregando...</CustomText>
+        </View>
     </View>
 );
 
@@ -14,12 +17,18 @@ export default LoadingScreen;
 const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
-    container: {
+    mainContainer: {
         flex: 1,
         justifyContent: 'center'
     },
+    borderContainer: {
+        borderRadius: 5,
+        backgroundColor: Colors.toRgba(Colors.darkMain, 0.7),
+        padding: 15
+    },
     text: {
         fontSize: FontSize.getSize('main', width) + 6,
-        fontWeight: '700'
+        fontWeight: '700',
+        color: Colors.accent500
     }
 })
